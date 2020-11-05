0ddd04e2becdc46850b1e775c864bccf26f01f6b
bigbreadguy <schadenlim@gmail.com>
2020-11-05 17:36:07 +0900

    crawl FlavorDB

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import requests
+import json
+
+cwd_dir = os.getcwd()
+db_dir = os.path.join(cwd_dir, "FlavorDB")
+
+if not os.path.exists(db_dir):
+    os.makedirs(db_dir)
+
+index = 0
+while True:
+    try:
+        url = "https://cosylab.iiitd.edu.in/flavordb/entities_json?id=%d" % index
+        data = requests.get(url).json()
+        obj = json.dumps(data)
+
+        file_name = os.path.join(db_dir, "%d.json" % index)
+
+        with open(file_name,'w') as f:
+            json.dump(data, f, indent=4, sort_keys=True)
+        
+        index+=1
+    except:
+        break
