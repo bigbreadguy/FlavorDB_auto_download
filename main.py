e1c9fb321946b7186d1fbb95b4055f132378de69
bigbreadguy <schadenlim@gmail.com>
2020-11-05 18:05:12 +0900

    Update

--- main.py
+++ main.py
@@ -27,5 +27,22 @@ while True:
         
         index+=1
     except:
-        print("Done")
-        break
+        try:
+            url = "https://cosylab.iiitd.edu.in/flavordb/entities_json?id=%d" % index + 1
+        data = requests.get(url).json()
+        obj = json.dumps(data)
+        
+        name_key = "entity_alias_readable"
+        name = data[name_key]
+
+        print("Downloading %s data" % name)
+
+        file_name = os.path.join(db_dir, "%d.json" % index + 1)
+
+        with open(file_name,'w') as f:
+            json.dump(data, f, indent=4, sort_keys=True)
+        
+        index+=1
+        except:
+            print("Done")
+            break
